68f12778312fcff5ff4265ea481d0b0cb9d086ac
smirandaudesa <smiranda@udesa.edu.ar>
2023-09-12 22:35:50 -0300

    subo componentes q faltan y screens

--- proyecto/src/components/Header/Header.js
+++ proyecto/src/components/Header/Header.js
@@ -1,49 +1,48 @@
+import { Link } from 'react-router-dom'
+import './styles.css'
+import React, { Component } from 'react'
 
-// import { Link } from 'react-router-dom'
-// import './styles.css'
-// import React, { Component } from 'react'
+ export default class Header extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
 
-//  export default class Header extends Component {
-//   constructor(props){
-//     super(props)
-//     this.state = {
+    }
+  }
+   render() {
+     return (
+       <nav className="nav">
+         <ul className="main-nav">
+           <li className="nav-item">
+             <Link to="/" className="nav-link" exact="true">
+               Home
+             </Link>
+           </li>
+           <li className="nav-item">
+             <Link to="/favoritos" className="nav-link">
+               Favoritos
+             </Link>
+           </li>
+           <li className="nav-item">
+             <Link to="/vercanciones" className="nav-link">
+               Todas Canciones
+             </Link>
+           </li>
+           <li className="nav-item">
+             <Link to="/veralbums" className="nav-link">
+               Todos Albums
+             </Link>
+           </li>
+         </ul>
 
-//     }
-//   }
-//    render() {
-//      return (
-//        <nav className="nav">
-//          <ul className="main-nav">
-//            <li className="nav-item">
-//              <Link to="/" className="nav-link" exact="true">
-//                Home
-//              </Link>
-//            </li>
-//            <li className="nav-item">
-//              <Link to="/favoritos" className="nav-link">
-//                Favoritos
-//              </Link>
-//            </li>
-//            <li className="nav-item">
-//              <Link to="/vercanciones" className="nav-link">
-//                Todas Canciones
-//              </Link>
-//            </li>
-//            <li className="nav-item">
-//              <Link to="/veralbums" className="nav-link">
-//                Todos Albums
-//              </Link>
-//            </li>
-//          </ul>
-
-//          <img className="foto-logo" src='/img/Logo.png' alt="logo"></img>
-//           <article className="header">
-//               <img className= 'header' src='/img/Header.png' alt="Logo de la web"></img>  
-//           </article>
+         <img className="foto-logo" src='/img/Logo.png' alt="logo"></img>
+          <article className="header">
+              <img className= 'header' src='/img/Header.png' alt="Logo de la web"></img>  
+          </article>
 
           
-//        </nav>
-//      )
-//    }
-//  }
+       </nav>
+     )
+   }
+ }
  
--- proyecto/src/screens/DetalleAlbum/index.js
+++ proyecto/src/screens/DetalleAlbum/index.js
@@ -1,116 +1,116 @@
-// import React, { Component } from "react";
-// import Footer from "../../components/Footer/Footer";
-// import Header from "../../components/Header/Header";
-// import Fomulario from '../../components/Fomulario/Formulario'
+import React, { Component } from "react";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import Fomulario from '../../components/Fomulario/Formulario'
 
-// class DetalleAlbum extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             album: "",
-//             favoritosAlbumes: []
-//         }
-//     }
+class DetalleAlbum extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            album: "",
+            favoritosAlbumes: []
+        }
+    }
 
-//     componentDidMount() {
-//         fetch(`https://thingproxy.freeboard.io/fetch/https://api.deezer.com/album/${this.props.match.params.id}`)
-//             .then((response) => response.json())
-//             .then((datos) =>
-//                 this.setState({
-//                     album: datos
-//                 }))
-//             .catch(error => console.log(error));
+    componentDidMount() {
+        fetch(`https://thingproxy.freeboard.io/fetch/https://api.deezer.com/album/${this.props.match.params.id}`)
+            .then((response) => response.json())
+            .then((datos) =>
+                this.setState({
+                    album: datos
+                }))
+            .catch(error => console.log(error));
 
-//         let favoritosStringAlbum = localStorage.getItem("favoritos-album")
-//         let favoritosArrayAlbum = JSON.parse(favoritosStringAlbum)
+        let favoritosStringAlbum = localStorage.getItem("favoritos-album")
+        let favoritosArrayAlbum = JSON.parse(favoritosStringAlbum)
 
-//         if (favoritosArrayAlbum === null) {
-//             favoritosArrayAlbum = []
-//         } else {
-//             this.setState({
-//                 favoritosAlbumes: favoritosArrayAlbum
-//             })
-//         }
-//     }
+        if (favoritosArrayAlbum === null) {
+            favoritosArrayAlbum = []
+        } else {
+            this.setState({
+                favoritosAlbumes: favoritosArrayAlbum
+            })
+        }
+    }
 
-//     agregarFavoritos(id, tipo) {
-//         if (!this.state.favoritosAlbumes.includes(id)) {
-//             let favoritosString = localStorage.getItem("favoritos-" + tipo)
-//             let favoritosArray = JSON.parse(favoritosString)
+    agregarFavoritos(id, tipo) {
+        if (!this.state.favoritosAlbumes.includes(id)) {
+            let favoritosString = localStorage.getItem("favoritos-" + tipo)
+            let favoritosArray = JSON.parse(favoritosString)
 
-//             if (favoritosArray === null) {
-//                 favoritosArray = []
-//             }
+            if (favoritosArray === null) {
+                favoritosArray = []
+            }
 
-//             favoritosArray.push(id)
+            favoritosArray.push(id)
 
-//             this.setState({
-//                 favoritosAlbumes: favoritosArray
-//             })
+            this.setState({
+                favoritosAlbumes: favoritosArray
+            })
 
-//             let favoritosStringNuevo = JSON.stringify(favoritosArray)
-//             localStorage.setItem("favoritos-" + tipo, favoritosStringNuevo)
+            let favoritosStringNuevo = JSON.stringify(favoritosArray)
+            localStorage.setItem("favoritos-" + tipo, favoritosStringNuevo)
 
-//             console.log(favoritosStringNuevo)
-//         }
-//     }
+            console.log(favoritosStringNuevo)
+        }
+    }
 
-//     quitarFavoritos(id, tipo) {
-//         if (this.state.favoritosAlbumes.includes(id)) {
-//             let favoritosString = localStorage.getItem("favoritos-" + tipo)
-//             let favoritosArray = JSON.parse(favoritosString)
+    quitarFavoritos(id, tipo) {
+        if (this.state.favoritosAlbumes.includes(id)) {
+            let favoritosString = localStorage.getItem("favoritos-" + tipo)
+            let favoritosArray = JSON.parse(favoritosString)
 
-//             if (favoritosArray === null) {
-//                 favoritosArray = []
-//             }
+            if (favoritosArray === null) {
+                favoritosArray = []
+            }
 
-//             let index = favoritosArray.indexOf(id)
-//             favoritosArray.splice(index, 1)
+            let index = favoritosArray.indexOf(id)
+            favoritosArray.splice(index, 1)
 
-//             this.setState({
-//                 favoritosAlbumes: favoritosArray
-//             })
+            this.setState({
+                favoritosAlbumes: favoritosArray
+            })
 
-//             let favoritosStringNuevo = JSON.stringify(favoritosArray)
-//             localStorage.setItem("favoritos-" + tipo, favoritosStringNuevo)
+            let favoritosStringNuevo = JSON.stringify(favoritosArray)
+            localStorage.setItem("favoritos-" + tipo, favoritosStringNuevo)
 
-//             console.log(favoritosStringNuevo)
-//         }
-//     }
+            console.log(favoritosStringNuevo)
+        }
+    }
 
-//     render() {
-//         return (
-//             <>
-//                 <Header />
-//                 <Fomulario />
+    render() {
+        return (
+            <>
+                <Header />
+                <Fomulario />
 
-//                 {
-//                     this.state.album !== "" ?
-//                     <section>
-//                         <img src={this.state.album.cover} alt={this.state.album.title} />
-//                         <h2>{this.state.album.title}</h2>
-//                         <p>Artista: {this.state.album.artist.name}</p>
-//                         <p>Nombre del disco: {this.state.album.title}</p>
-//                         <p>Listado de canciones:</p>
-//                         {this.state.album.tracks.data.map((cancion, i) => (
-//                             <div key={cancion + i}>
-//                                 <ul ><li >{cancion.title}</li></ul>
-//                             </div>
-//                         ))}
-//                         {
-//                             this.state.favoritosAlbumes.includes(this.props.id) ?
-//                             <button onClick={() => this.quitarFavoritos(this.props.id, "album")}>Quitar de favoritos</button> :
-//                             <button onClick={() => this.agregarFavoritos(this.props.id, "album")}>Agregar a favoritos</button>
-//                         }
-//                     </section> :
-//                     <h2>Cargando...</h2>
-//                 }
-//                 <Footer />
+                {
+                    this.state.album !== "" ?
+                    <section>
+                        <img src={this.state.album.cover} alt={this.state.album.title} />
+                        <h2>{this.state.album.title}</h2>
+                        <p>Artista: {this.state.album.artist.name}</p>
+                        <p>Nombre del disco: {this.state.album.title}</p>
+                        <p>Listado de canciones:</p>
+                        {this.state.album.tracks.data.map((cancion, i) => (
+                            <div key={cancion + i}>
+                                <ul ><li >{cancion.title}</li></ul>
+                            </div>
+                        ))}
+                        {
+                            this.state.favoritosAlbumes.includes(this.props.id) ?
+                            <button onClick={() => this.quitarFavoritos(this.props.id, "album")}>Quitar de favoritos</button> :
+                            <button onClick={() => this.agregarFavoritos(this.props.id, "album")}>Agregar a favoritos</button>
+                        }
+                    </section> :
+                    <h2>Cargando...</h2>
+                }
+                <Footer />
 
-//             </>
-//         )
-//     }
+            </>
+        )
+    }
 
-// }
+}
 
-// export default DetalleAlbum;
+export default DetalleAlbum;
